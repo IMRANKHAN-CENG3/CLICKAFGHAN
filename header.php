9ac5e77c55728ce29997a018bc14a244fe424a59
IMRANKHAN-CENG3 <60873731+IMRANKHAN-CENG3@users.noreply.github.com>
2020-12-23 21:15:08 +0430

    Add files via upload

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap-4.0.0/bootstrap.min.css">
+</head>
+<body>
+<header>
+	<div class="headerTop">
+		<div class="navbar  navbar-fixed-top" id="nav">
+			<div class="cotainer-fluid" >
+				<div class="row" >
+					<div class="col-sm-2" style="background-color:">
+
+						<img src="img/DSC_0273 copy.jpg" class="profyle">
+						<p style="color: #fff; margin-left: 1%">imran khan</p>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AfghanClick</title>
+	<meta charset="utf-8">
+	<meta name="vieweport" content="width=device-width">
+
+</head>
+<body>
+	<?php require_once('header.php') ?>
+<h1>hello</h1>
+</body>
+</html>
