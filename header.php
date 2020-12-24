94e04776a053a17acc162724a93df94166af2bb0
IMRANKHAN-CENG3 <60873731+IMRANKHAN-CENG3@users.noreply.github.com>
2020-12-24 18:02:02 +0430

    Update header.php
    login and register button added

--- header.php
+++ header.php
@@ -4,23 +4,35 @@
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" type="text/css" href="bootstrap-4.0.0/bootstrap.min.css">
+	
+	<meta name="viewport" content="width=device-width"> 
 </head>
 <body>
 <header>
 	<div class="headerTop">
 		<div class="navbar  navbar-fixed-top" id="nav">
 			<div class="cotainer-fluid" >
-				<div class="row" >
-					<div class="col-sm-2" style="background-color:">
+				<div class="row"  >
+					<div class="col-sm-2">
 
 						<img src="img/DSC_0273 copy.jpg" class="profyle">
 						<p style="color: #fff; margin-left: 1%">imran khan</p>
 					</div>
-					
+				
+						<div class="col-sm-2" style="margin-left: 50%;margin-top: 2%;">
+							
+							<a class="" href="" style="color: #fff; margin-right: 10%;">Register</a>
+							<a href="" id="Login" style="color: #fff">Login</a>
+						</div>
+						<p style="color: #fff">Search</p>
+						<div class="col-sm-6" style=" margin-bottom: 10%">
+							<input type="" name="">
+						</div>
 				</div>
+				
 			</div>
 		</div>
 	</div>
 </header>
 </body>
-</html>
+</html>>
