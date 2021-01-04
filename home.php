fc117457abee4cef3f3f96990cc0a6f3de3138ab
imran khan <60873731+IMRANKHAN-CENG3@users.noreply.github.com>
2021-01-04 15:44:52 +0430

    Update home.php

--- home.php
+++ home.php
@@ -3,11 +3,74 @@
 <head>
 	<title>AfghanClick</title>
 	<meta charset="utf-8">
-	<meta name="vieweport" content="width=device-width">
+	<meta name="viewport" content="width=device-width"> 
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="home.css">
+
 
 </head>
+
 <body>
+  
 	<?php require_once('header.php') ?>
-<h1>hello</h1>
+		<div class="container-fluid">
+ 
+  <div class="row">
+ <div class="col-lg-10" style=" background-color: #fff; margin-top: 2%" >
+ 	<div class="w3-row">
+
+
+  <div class="w3-col m3  w3-center"  style="margin:  1%">
+  	
+<div class="w3-card-4">
+  <img id ="img"src="img/11.jpg" alt="Person" style="max-width:100%;height: 200px">
+  <div class="w3-container" style="display: inline">
+    <h4 style="text-align: right;"><b>بوت مردانه</b></h4>
+    <p  id ="p"style="text-align: right;"><button class="dbtn" >kdjk</button>فعمت   </p>
+  </div>
+</div>
+
+  </div>
+    <div class="w3-col m3  w3-center"  style="margin:  1%">
+  	
+<div class="w3-card-4">
+  <img src="img/11.jpg" alt="Person" style="max-width:100%;height: 200px;">
+  <div class="w3-container">
+    <h4 style="text-align: right;"><b>بوټ مردانه </b></h4>
+    <p style="text-align: right;">قعمت: ۲۰۰۰</p>
+  </div>
+</div>
+
+  </div>
+  	 <div class="w3-col m2  w3-right"  style="margin:  1%">
+  	 	<ul class="w3-ul w3-card-4" style="direction: rtl">
+  <li>لاسی صنایع</li>
+  <li>پوښاک</li>
+  <li>الکترونکی الات</li>
+   <li>لاسی صنایع</li>
+  <li>پوښاک</li>
+  <li>الکترونکی الات</li>
+   <li>لاسی صنایع</li>
+  <li>پوښاک</li>
+  <li>الکترونکی الات</li>
+   <li>لاسی صنایع</li>
+  <li>پوښاک</li>
+  <li>الکترونکی الات</li>
+   <li>لاسی صنایع</li>
+  <li>پوښاک</li>
+  <li>الکترونکی الات</li>
+  <li>لاسی صنایع</li>
+  <li>پوښاک</li>
+  <li>الکترونکی الات</li>
+</ul>
+  	 </div>
+
+  
+</div>
+
+ </div>
+  
+  </div>
+</div>
 </body>
 </html>
