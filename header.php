44f5eff79a748fe66bfe13ddfa85465773efa615
imran khan <60873731+IMRANKHAN-CENG3@users.noreply.github.com>
2021-01-04 15:44:15 +0430

    Update header.php

--- header.php
+++ header.php
@@ -1,35 +1,51 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>AfghanClic</title>
+		<title></title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" type="text/css" href="bootstrap-4.0.0/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="w3.css">
+    <script type="text/javascript" src="bootstrap-4.0.0/bootstrap.min.js"></script>
+    <script type="text/javascript" src="bootstrap-4.0.0/jquery-1.11.3.min.js"></script>
 	
 	<meta name="viewport" content="width=device-width"> 
 </head>
 <body>
 <header>
 	<div class="headerTop">
-		<div class="navbar  navbar-fixed-top" id="nav">
-			<div class="cotainer-fluid" >
-				<div class="row"  >
-					<div class="col-sm-2">
+	<div class="container-fluid">
+  
+  <div class="row" style="margin-top: 2%">
+    <div class="col-sm-4" style="">
+    	<img class="profyle" style="margin-right: 80%" src="img/DSC_0273 copy.jpg" >
+    	<p style="margin-right: 78%; color: #fff">Imrna Khan</p>
+    </div>
+    <div class="col-sm-4 w3-hide-small w3-hide-medium"  style="">
+    	   <input class="d-none d-lg-block w3-input w3-border w3-hover-white  " placeholder="search" type="text" style="background-color: #ff1a75; color: white">
+    
+    </div>
+    <div class="col-sm-4  " style="">
+    	
+    	<a  href="" style="color: #fff;margin-right: 5%">Register</a>
+    	<a href="" style="color: #fff; margin-right: 10%;">Login</a>
+    </div>
+    
 
-						<img src="img/DSC_0273 copy.jpg" class="profyle">
-						<p style="color: #fff; margin-left: 1%">imran khan</p>
-					</div>
-				
-						<div class="col-sm-2" style="margin-left: 50%;margin-top: 2%;">
-							
-							<a class="" href="" style="color: #fff; margin-right: 10%;">Register</a>
-							<a href="" id="Login" style="color: #fff">Login</a>
-						</div>
-					
-				</div>
-				
-			</div>
-		</div>
+  </div>
+</div>
+
+</div>
+
 	</div>
 </header>
+<body>
+
+
+<script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
 </body>
-</html>>
+</html>
